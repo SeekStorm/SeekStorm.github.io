6e4163698e00f9f80c7649b827126d4bb1e21c88
Wolf Garbe <wolf.garbe@t-online.de>
2025-10-23 15:14:45 +0200

    benchmark results updated

--- search-benchmark-game-singlefield/service-worker.js
+++ search-benchmark-game-singlefield/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b83eae4e2e4343e988370e38597b789.js"
+  "./precache-manifest.4877fb426cff0e5e3e85119ae25212cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
